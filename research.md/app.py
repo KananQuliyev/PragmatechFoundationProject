3ac52dbbdab475029e341ee950c47133b07b1009
Kanan <KananQuliyev3128@yandex.com>
2022-04-05 15:35:10 +0400

    Python yeni

--- research.md/app.py
+++ research.md/app.py
@@ -1,5 +1,26 @@
-x = "Python"
-y = "Welcome!"
-
+x=4 
 print(x)
-print(y)
+x="Kanan"
+print(x)
+x=4 
+print(type(x))
+x="Kanan"
+print(type(x))
+x=4.4
+print(type(x))
+x=9
+y=5
+print(x*y)
+x=9
+y=5
+print(type(x*y))
+x = "Kanan"
+print(x[1])
+for x in "Kanan":
+ print(x)
+ x=111
+ y=11
+ if x>y:
+     print("x is greater than y")
+ else:
+     print("x is not  greater than y")
